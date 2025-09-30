65d8b6db9bea62a32fe4d6cc8f4478235e87cf8e
Hugo <48035284+Brokelind@users.noreply.github.com>
2025-09-30 19:57:26 +0200

    Update tradingmodelsystem.py

--- tradingmodelsystem.py
+++ tradingmodelsystem.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 import numpy as np
 import pandas as pd
 import joblib
-import pandas_ta_classic as ta
+import pandas_ta as ta
 from scipy.stats import norm
 from sklearn.ensemble import RandomForestRegressor, VotingRegressor
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
